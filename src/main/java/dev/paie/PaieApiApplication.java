0d9638dcc6060df19c7dd0f9f2fb6c83bed137b5
jbmerand <jbmerand@icloud.com>
2019-09-02 09:58:26 +0200

    premier commit

---
+++ src/main/java/dev/paie/PaieApiApplication.java
@@ -0,0 +1,13 @@
+package dev.paie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaieApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaieApiApplication.class, args);
+	}
+
+}
